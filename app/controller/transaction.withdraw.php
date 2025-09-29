f092296f9e8afe6260916befb68ac0cece1bce60
Tijani Moro <tjhackx111@gmail.com>
2025-09-29 20:03:19 +0000

    initial commit

--- app/controller/transaction.withdraw.php
+++ app/controller/transaction.withdraw.php
@@ -1,72 +1,85 @@
-  <?php
-    // upload today collection file
-
+<?php
     require ('../../system/DatabaseConnector.php');
-    
+
+    $errors = null;
     $message = null;
-    $status = null;
-    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['upload_date'])) {
-        $uploadDate = $_POST['upload_date'] ?? null;
-        $totalcollected = $_POST['totalcollected'] ?? 0;
-        $note = sanitize($_POST['note']); // 
 
-        // check if today date is already uploaded
-        $check_sql = "SELECT * FROM daily_collections WHERE daily_collection_date = ? LIMIT 1";
-        $check_stmt = $dbConnection->prepare($check_sql);
-        $check_stmt->execute([$uploadDate]);
-        if ($check_stmt->rowCount() > 0) {
-            $message = "Collection for this date is already uploaded.";
+    $added_by = null;
+    $added_by_id = $admin_id;
+    if (admin_has_permission()) {
+        $added_by = 'admin';
+    } else {
+        $errors = 'You don\'t have permission to take this action !';
+    }
+
+    // check if is posted
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['withdrawal_select_customer'])) {
+
+        // get form data
+        $customer_info = sanitize($_POST['withdrawal_select_customer']);
+        list($customer_name, $customer_account_number) = explode(',', $customer_info);
+        $transaction_balance = sanitize($_POST['customer_balance']);
+        $transaction_amount = sanitize($_POST['amount-to-withdraw']);
+        $payment_mode = sanitize($_POST['withdrawal_payment_mode']); 
+        $transaction_date = sanitize($_POST['withdrawal_today_date']);
+        $transaction_note = sanitize($_POST['withdrawal_note']);
+
+        $find_customer_row = findCustomerByAccountNumber($customer_account_number);
+        if (!$find_customer_row) {
+            $errors = 'Customer not found !';
         }
 
-        // Validate file upload
-        if (!isset($_FILES['collection_file']) || $_FILES['collection_file']['error'] !== UPLOAD_ERR_OK) {
-            $message = "No file uploaded.";
-            
+        // account balance
+        $a = getCustomerBalance(0, $account_number);
+        $b = $a['balance'];
+
+        if ((float)$transaction_balance !== (float)$b) {
+            $errors = 'Balance has been temptered !';
         }
 
-        $file = $_FILES['collection_file'];
-        $allowedTypes = ['image/png', 'image/jpeg'];
-        $maxSize = 6 * 1024 * 1024; // 6MB
-
-        if (!in_array($file['type'], $allowedTypes)) {
-            $message = "Only PNG and JPG files are allowed.";
+        if ((float)$transaction_amount > (float)$b) {
+            $errors = 'Amount to withdraw is greater that balance available !';
         }
 
-        if ($file['size'] > $maxSize) {
-            $message = "File size exceeds 6MB.";
-        }
-
-        $uploadDir =  '../../assets/media/uploads/collection-files/';
-        if (!is_dir($uploadDir)) {
-            mkdir($uploadDir, 0755, true);
-        }
-
-        $filename = basename($file['name']);
-        
-        $targetPath = $uploadDir . $filename;
-        $targetPath = $uploadDir . time() . '_' . preg_replace('/[^a-zA-Z0-9._-]/', '_', $filename); // prevent overwriting and sanitize filename
-
-        $uniqueid = guidv4() . '-' . strtotime(date('Y-m-d H:i:s'));
-
-        if ($message == null || $message == '') {
-            if (move_uploaded_file($file['tmp_name'], $targetPath)) {
-                // insert into daily_collections
-                $sql = "INSERT INTO daily_collections (daily_id, daily_collector_id, daily_collection_date, daily_total_collected, daily_proof_image, daily_note) VALUES (?, ?, ?, ?, ?, ?)";
-                $stmt = $dbConnection->prepare($sql);
-                $result = $stmt->execute([$uniqueid, $admin_id, $uploadDate, $totalcollected, $targetPath, $note]);
-                if (!$result) {
-                    $message = "Database error: Could not save file info.";
-                }
-            } else {
-                $message = "File upload failed.";
+        // validate inputs
+        $required = array('withdrawal_select_customer', 'amount-to-withdraw', 'withdrawal_payment_mode', 'withdrawal_today_date');
+        foreach ($required as $f) {
+            if (empty($f)) {
+                $errors = $f . ' is required !';
+                break;
             }
         }
 
-        if ($message) {
-            $status = 'error';
-        } else {
-            $status = 'success';
-            $message = "File uploaded successfully for date: " . sanitize($uploadDate);
-        }
-        echo json_encode(['status' => $status, 'message' => $message]);
+        $unique_id = guidv4() . '-' . strtotime(date("Y-m-d H:i:s"));
+
+        // insert into database
+        $stmt = $dbConnection->prepare("INSERT INTO withdrawals (withdrawal_id, withdrawal_customer_id, withdrawal_customer_account_number, withdrawal_account_balance, withdrawal_approver_id, withdrawal_amount_requested, withdrawal_date_requested, withdrawal_note, withdrawal_mode) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            // check if there are no error before inserting
+            if ($errors) {
+                // do nothing
+            } else {
+            
+                $stmt->execute([$unique_id, $find_customer_row->customer_id, $customer_account_number, $admin_id, $transaction_amount, $transaction_date, $transaction_note, $payment_mode]);
+
+                if ($stmt) {
+                    // 
+                    $log_message = ucwords($added_by) . ' [' . $added_by_id . '] made new withdrawal transaction for ' . ucwords($customer_name) . ' (' . $customer_account_number . ') account';
+                    add_to_log($log_message, $added_by_id, $added_by);
+
+                    $message = 'Withdrawal transaction added successfully.';
+                } else {
+                    $errors = 'An error occurred. Please try again.';
+                }
+            }
+        
+    } else {
+        $errors = 'Invalid request.';
     }
+
+    if ($errors) {
+        echo json_encode(['status' => 'error', 'message' => $errors]);
+    } else {
+        echo json_encode(['status' => 'success', 'message' => $message]);
+    }
+?>
